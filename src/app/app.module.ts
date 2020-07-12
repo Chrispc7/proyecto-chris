927d475f1dab4800984387b925fcc47bde7c3fdb
Chrispc7 <67751458+Chrispc7@users.noreply.github.com>
2020-07-12 01:01:50 -0400

    Configuraciones Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+/* FIREBASE */
+import { AngularFireModule } from'@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment';
 
+const routes: Routes = [];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
+    RouterModule.forRoot(routes)
   ],
+  exports:[RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyC6XB8SMdK3DUrwIvp586mOHeBo03ZaeRI",
+    authDomain: "chris-store-b50a1.firebaseapp.com",
+    databaseURL: "https://chris-store-b50a1.firebaseio.com",
+    projectId: "chris-store-b50a1",
+    storageBucket: "chris-store-b50a1.appspot.com",
+    messagingSenderId: "160757464293",
+    appId: "1:160757464293:web:7edde48ff2aefca23b280c",
+    measurementId: "G-6VBYCW0ZYB"
+  }
 };
 
 /*
